842af1999b1fe489062f79559d3dc32bc787640b
YuKaocgu <yu.kao@cgu.edu>
2017-09-12 18:07:59 -0700

    Add files via upload

---
+++ applicant.py
@@ -0,0 +1,70 @@
+class applicant:
+    def __init__(self, r_id , rday, camp, name,age,essay,payment,recording,r_status,dorm,band,talent,instrument,pair):
+        self.r_id = r_id
+        self.rday = rday
+        self.camp = camp
+        self.name = name
+        self.age = age
+        self.essay = essay
+        self.payment = payment
+        self.recording = recording
+        self.r_status = r_status
+        self.dorm = dorm
+        self.band = band
+        self.talent = talent
+        self.instrument = instrument
+        self.pair = pair
+    
+    def getDay(slef):
+        return self.rday
+    def getCamp(self):
+        return self.camp
+    def getName(self):
+        return self.name
+    def getAge(self):
+        return self.age
+    def getEssay(self):
+        return self.essay
+    def getPayment(self):
+        return self.payment
+    def getRecording(self):
+        return self.recording
+    def getStatus(self):
+        return self.r_status
+    def getDorm(slef):
+        return self.dorm
+    def getBand(self):
+        return self.band
+    def getTalent(self):
+        return self.talent
+    def getInstrument(self):
+        return self.instrument
+    def getPair(self):
+        return self.pair
+    def setDay(slef,new_day):
+        self.rday = new_day
+    def getCamp(self, new_camp):
+        self.camp = new_camp
+    def getName(self,new_name):
+        self.name = new_name
+    def getAge(self,new_age):
+        self.age = new_age
+    def getEssay(self,new_essay):
+        self.essay = new_essay
+    def getPayment(self,new_payment):
+        self.payment = new_payment
+    def getRecording(self,new_recording):
+        self.recording = new_recording
+    def getStatus(self,new_status):
+        self.r_status = new_status
+    def getDorm(slef,new_dorm):
+        self.dorm = new_dorm
+    def getBand(self,new_band):
+        self.band = new_band
+    def getTalent(self,new_talent):
+        self.talent = new_talent
+    def getInstrument(self,new_instrument):
+        self.instrument = new_instrument
+    def getPair(self,new_pair):
+        self.pair = new_pair
+    
