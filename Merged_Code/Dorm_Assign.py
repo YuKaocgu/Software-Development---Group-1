655fee9896ccb08d63966045ddd6100a328378e4
YuKaocgu <yu.kao@cgu.edu>
2017-10-10 15:47:52 -0700

    Add files via upload

---
+++ Merged_Code/Dorm_Assign.py
@@ -0,0 +1,45 @@
+import numpy
+def isFull(dorm):
+    if (len(dorm) == 8):
+        return True
+    else:
+        return False
+def avg(data_List):
+    if len(data_List) == 0:
+        return 0
+    else:
+        return sum(data_List)/len(data_List)
+
+def dorm_assign(Dorm1,Dorm2,Dorm3,age):
+    Dorm_Status = [isFull(Dorm1),isFull(Dorm2),isFull(Dorm3)]
+    if Dorm_Status == [False,False,False]:
+        check_list = [ abs(avg(Dorm1)-age),abs(avg(Dorm2)-age),abs(avg(Dorm3)-age)]
+        if max(check_list) == abs(avg1-age):
+            return 1
+        elif max(check_list) == abs(avg2-age):
+            return 2
+        else:
+            return 3
+    elif Dorm_Status == [True,False,False]:
+        if abs(avg(Dorm2)-age) > abs(avg(Dorm3)-age):
+            return 2
+        else: 
+            return 3
+    elif Dorm_Status == [False,True,False]:
+        if abs(avg(Dorm1)-age) > abs(avg(Dorm3)-age):
+            return 1
+        else: 
+            return 3
+    elif Dorm_Status == [False,False,True]:
+        if abs(avg(Dorm1)-age) > abs(avg(Dorm2)-age):
+            return 1
+        else: 
+            return 2
+    elif Dorm_Status == [False,True,True]:
+        return 1
+    elif Dorm_Status == [True,False,True]:
+        return 2
+    elif Dorm_Status == [True,True,False]:
+        return 3
+
+
