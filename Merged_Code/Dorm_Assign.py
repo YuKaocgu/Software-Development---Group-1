969ec1d89f14c38de9168c0fba57514004fcc786
YuKaocgu <yu.kao@cgu.edu>
2017-10-10 18:23:10 -0700

    Update Dorm_Assign.py

--- Merged_Code/Dorm_Assign.py
+++ Merged_Code/Dorm_Assign.py
@@ -1,4 +1,12 @@
 import numpy
+def test():
+    age = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+    Dorm1 = []
+    Dorm2 = []
+    Dorm3 = []
+    for i in age:
+        result = dorm_assign(Dorm1,Dorm2,Dorm3,i)
+        print(result)
 def isFull(dorm):
     if (len(dorm) == 8):
         return True
@@ -15,31 +23,43 @@ def dorm_assign(Dorm1,Dorm2,Dorm3,age):
     if Dorm_Status == [False,False,False]:
         check_list = [ abs(avg(Dorm1)-age),abs(avg(Dorm2)-age),abs(avg(Dorm3)-age)]
         if max(check_list) == abs(avg(Dorm1)-age):
+            Dorm1.append(age)
             return 1
         elif max(check_list) == abs(avg(Dorm2)-age):
+            Dorm2.append(age)
             return 2
         else:
+            Dorm3.append(age)
             return 3
     elif Dorm_Status == [True,False,False]:
         if abs(avg(Dorm2)-age) > abs(avg(Dorm3)-age):
+            Dorm2.append(age)
             return 2
-        else: 
+        else:
+            Dorm3.append(age) 
             return 3
     elif Dorm_Status == [False,True,False]:
         if abs(avg(Dorm1)-age) > abs(avg(Dorm3)-age):
+            Dorm1.append(age)
             return 1
-        else: 
+        else:
+            Dorm3.append(age) 
             return 3
     elif Dorm_Status == [False,False,True]:
         if abs(avg(Dorm1)-age) > abs(avg(Dorm2)-age):
+            Dorm1.append(age)
             return 1
-        else: 
+        else:
+            Dorm2.append(age) 
             return 2
     elif Dorm_Status == [False,True,True]:
+        Dorm1.append(age)
         return 1
     elif Dorm_Status == [True,False,True]:
+        Dorm2.append(age)
         return 2
     elif Dorm_Status == [True,True,False]:
+        Dorm3.append(age)
         return 3
 
 
