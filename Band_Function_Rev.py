10d62f4c8710e516553c9903420b872772b5fab7
YuKaocgu <yu.kao@cgu.edu>
2017-10-29 17:51:39 -0700

    Add files via upload

---
+++ Band_Function_Rev.py
@@ -0,0 +1,51 @@
+def test():
+    b1 = {}
+    b1_gender = []
+
+def checkInstrument(band,ins):
+    if ins not in band.keys():
+        return True
+    else:
+        return False
+
+def checkTalent(band,talent):
+    if countTalent(band,talent) < 2:
+        return True
+    else:
+        return False
+
+def checkGender(band_gender,gender):
+    if countGender(band_gender,gender) < 3:
+        return True
+    else:
+        return False
+
+def countTalent(band,talent):
+    result = countIf(band.values(),talent)
+    return result
+
+def countGender(band_gender,gender):
+    count = 0
+    for g in band_gender:
+        if g == gender:
+            count += 1
+    return count
+
+def sumOfTalent(band):
+    result = 0
+    for i in band.keys():
+        result += band[i]
+    return result
+
+def countIf(list,x):
+    count = 0
+    for i in list:
+        if x == i:
+            count += 1
+    return count
+
+def getLetters(letter):
+    if 'a' <= letter <= 'z':
+        return chr(ord(letter)-32),letter
+    else:
+        return letter,chr(ord(letter)+32)
